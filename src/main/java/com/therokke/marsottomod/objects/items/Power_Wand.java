f41b32e7588f4b12e2f46b0e821c5f219565e168
MCModding <nicolamarsotto9.nm@gmail.com>
2020-11-10 12:20:44 +0100

    Power_Wand

--- src/main/java/com/therokke/marsottomod/objects/items/Power_Wand.java
+++ src/main/java/com/therokke/marsottomod/objects/items/Power_Wand.java
@@ -1,33 +1,37 @@
 package com.therokke.marsottomod.objects.items;
 
-import com.therokke.marsottomod.MarsottoMod;
 import com.therokke.marsottomod.util.RegistryHandler;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.entity.projectile.PotionEntity;
-import net.minecraft.item.Item;
+import net.minecraft.item.IItemTier;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
+import net.minecraft.item.SwordItem;
 import net.minecraft.potion.Effect;
 import net.minecraft.potion.EffectInstance;
 import net.minecraft.potion.Effects;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
-import net.minecraft.world.Explosion;
 import net.minecraft.world.World;
-import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 
-public class SpecialItem extends Item {
-    public SpecialItem() {
-        super(new Item.Properties().group(MarsottoMod.TAB));
+import java.util.Random;
+
+public class Power_Wand extends SwordItem {
+
+    Effect[] effectList = {Effects.ABSORPTION,Effects.HASTE,Effects.INSTANT_HEALTH};
+
+    public Power_Wand(IItemTier tier, int attackDamageIn, float attackSpeedIn, Properties builder) {
+        super(tier, attackDamageIn, attackSpeedIn, builder);
     }
 
     @Override
     public ActionResult<ItemStack> onItemRightClick(World worldIn, PlayerEntity playerIn, Hand handIn) {
         ItemStack item = playerIn.getHeldItemMainhand();
-        playerIn.addPotionEffect(new EffectInstance(Effects.JUMP_BOOST,200,2));
-        if(playerIn.inventory.hasItemStack(new ItemStack(RegistryHandler.SPECIAL_ITEM.get()))){
-            item.damageItem(50,playerIn,playerEntity -> {});
-        }
+
+        Random random = new Random();
+        int i =(int) Math.random() * 4;
+
+        playerIn.addPotionEffect(new EffectInstance(effectList[i],200,2));
+        item.damageItem(15,playerIn,playerEntity -> {});
+
         return super.onItemRightClick(worldIn, playerIn, handIn);
     }
 }
--- src/main/java/com/therokke/marsottomod/util/RegistryHandler.java
+++ src/main/java/com/therokke/marsottomod/util/RegistryHandler.java
@@ -1,8 +1,10 @@
 package com.therokke.marsottomod.util;
 
 import com.therokke.marsottomod.MarsottoMod;
-import com.therokke.marsottomod.objects.items.SpecialItem;
+import com.therokke.marsottomod.objects.items.Power_Wand;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemTier;
+import net.minecraft.item.SwordItem;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
@@ -20,9 +22,10 @@ public class RegistryHandler {
     public static final RegistryObject<Item> RUBY = ITEMS.register("ruby",() -> new Item(new Item.Properties().group(MarsottoMod.TAB)));
 
     //Tools
+    public static final RegistryObject<SwordItem> POWER_WAND = ITEMS.register("power_wand",()->
+            new Power_Wand(ItemTier.IRON,1,1,new Item.Properties().group(MarsottoMod.TAB)));
 
     //Custom Items
-    public static final RegistryObject<Item> SPECIAL_ITEM = ITEMS.register("special_item", SpecialItem::new);
 
 
 }
