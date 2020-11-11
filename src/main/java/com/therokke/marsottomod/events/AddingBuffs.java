f008872fa3e464dcb468287cdb7fd0b5cddeb8fc
MCModding <nicolamarsotto9.nm@gmail.com>
2020-11-11 10:42:06 +0100

    Update AddingBuffs.java

--- src/main/java/com/therokke/marsottomod/events/AddingBuffs.java
+++ src/main/java/com/therokke/marsottomod/events/AddingBuffs.java
@@ -6,7 +6,9 @@ import com.therokke.marsottomod.objects.items.Power_Wand;
 import com.therokke.marsottomod.util.RegistryHandler;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.potion.Effects;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
@@ -18,9 +20,9 @@ import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
 public class AddingBuffs {
 
     @SubscribeEvent
-    public void addInstaHealth(PlayerInventory e){
-        if(e.hasItemStack(new ItemStack(RegistryHandler.INSTANT_HEALTH_ITEM.get()))){
-            Power_Wand.add(Effects.INSTANT_HEALTH);
+    public void addInstaHealth(PlayerInventory player){
+        if(player.hasItemStack(new ItemStack(Items.PUMPKIN))){
+            System.out.println("Ce lo ho");
         }
     }
 
